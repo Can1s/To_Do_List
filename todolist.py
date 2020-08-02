3d0b3096866838df75bf05fdd2991fd9fda05be6
Erdaulet <47812867+Can1s@users.noreply.github.com>
2020-08-02 15:42:11 +0600

    Stage 3/4
    Complete third stage

--- todolist.py
+++ todolist.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Date
-from datetime import datetime
+from datetime import datetime, timedelta
 from sqlalchemy.orm import sessionmaker
 
 engine = create_engine('sqlite:///todo.db?check_same_thread=False')  # Create file of DB
@@ -26,8 +26,9 @@ session = Session()
 
 
 def print_today_tasks():
-    tasks = session.query(Table).all()  # a list of tasks
-    print("Today:")
+    today = datetime.today()
+    tasks = session.query(Table).filter(Table.deadline == today.date()).all()  # a list of tasks
+    print(f"Today {today.strftime('%d %b')}:")
     if len(tasks) == 0:
         print("Nothing to do!")
     else:
@@ -36,10 +37,34 @@ def print_today_tasks():
     print()
 
 
+def print_weeks_tasks():
+    for i in range(7):
+        date = datetime.today().date() + timedelta(days=i)
+        tasks = session.query(Table).filter(Table.deadline == date).all()
+        print(f"{date.strftime('%A %d %b')}:")
+        if len(tasks) == 0:
+            print("Nothing to do!")
+        else:
+            for j in range(len(tasks)):
+                print(f"{j + 1}. {tasks[j].task}")
+        print()
+
+
+def print_all_tasks():
+    print("All tasks:")
+    tasks = session.query(Table.task, Table.deadline).order_by(Table.deadline).all()
+    for i in range(len(tasks)):
+        print(f"{i+1}. {tasks[i][0]}. {tasks[i][1].strftime('%#d %b')}")
+    print()
+
+
 def add_task():
     print("Enter task")
     new_task = input()
-    new_row = Table(task=new_task)
+    print("Enter deadline")
+    year, month, day = input().split("-")
+    new_deadline = datetime(int(year), int(month), int(day))
+    new_row = Table(task=new_task, deadline=new_deadline)
     session.add(new_row)
     session.commit()
     print("The task has been added!")
@@ -48,7 +73,9 @@ def add_task():
 
 def print_menu():
     print("""1) Today's tasks
-2) Add task
+2) Week's tasks
+3) All tasks
+4) Add task
 0) Exit""")
 
 
@@ -59,6 +86,10 @@ def main():
         if action == 1:
             print_today_tasks()
         elif action == 2:
+            print_weeks_tasks()
+        elif action == 3:
+            print_all_tasks()
+        elif action == 4:
             add_task()
         elif action == 0:
             print()
